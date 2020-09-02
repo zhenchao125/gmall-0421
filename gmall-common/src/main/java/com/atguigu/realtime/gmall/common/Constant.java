5a028f7621c2b25f58efe9ff375ea5e0f50b1978
zhiling <zhiling@atguigu.com>
2020-09-02 14:47:54 +0800

    更新

---
+++ gmall-common/src/main/java/com/atguigu/realtime/gmall/common/Constant.java
@@ -0,0 +1,12 @@
+package com.atguigu.realtime.gmall.common;
+
+/**
+ * @Author lzc
+ * @Date 2020/9/2 14:36
+ */
+public class Constant {
+
+    public static final String STARTUP_TOPIC = "startup_topic";
+    // shift + ctrl + u 大小写切换
+    public static final String EVENT_TOPIC = "event_topic";
+}
--- gmall-log/src/main/java/com/atguigu/realtime/gmalllog/PublisherController.java
+++ gmall-log/src/main/java/com/atguigu/realtime/gmalllog/PublisherController.java
@@ -2,6 +2,11 @@ package com.atguigu.realtime.gmalllog;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.atguigu.realtime.gmall.common.Constant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,13 +23,39 @@ public class PublisherController {
     public String doLog(String log) {
         // 1. 添加时间戳
         log = addTs(log);
-        System.out.println(log);
-        // 2. 落盘(给离线需求使用)
-
+        // 2. 落盘(给离线需求使用)   log4j  logging
+        saveToDisk(log);
         // 3. 把数据发动到kafka
-
+        sendToKafka(log);
 
         return "ok";
+    }
+
+    @Autowired  // 自动注入
+            KafkaTemplate<String, String> kafka;
+
+    /**
+     * 把数据发送到kafka
+     *
+     * @param log
+     */
+    private void sendToKafka(String log) {
+        // 启动日志和事件日志, 分别进入不同的topic
+        if (log.contains("startup")) {
+            kafka.send(Constant.STARTUP_TOPIC, log);
+        } else {
+            kafka.send(Constant.EVENT_TOPIC, log);
+        }
+    }
+
+    /**
+     * 把日志写入磁盘
+     *
+     * @param log
+     */
+    private void saveToDisk(String log) {
+        Logger logger = LoggerFactory.getLogger(PublisherController.class);
+        logger.info(log);
     }
 
     /**
